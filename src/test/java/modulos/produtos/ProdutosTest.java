f3fe0969d28ff4cbbff47306ee0d7b4705040d08
dinosaulo <sab2@cin.ufpe.br>
2022-05-15 00:51:13 -0300

    esturutrando o a automação de testes e adicionando o  primeiro caso de testes

---
+++ src/test/java/modulos/produtos/ProdutosTest.java
@@ -0,0 +1,56 @@
+package modulos.produtos;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import paginas.LoginPage;
+
+import java.time.Duration;
+
+@DisplayName("Testes WEB do módulo de produtos")
+public class ProdutosTest {
+
+    public WebDriver navegador;
+
+    @BeforeEach
+    public void beforeEach(){
+        // Abrir o navegador
+        System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver101\\chromedriver.exe");
+        this.navegador = new ChromeDriver();
+
+        // vou maximizar a tela
+        navegador.manage().window().maximize();
+
+        // Vou definir um tempo de espera padrão de 1 segundo
+        navegador.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+
+        // Navegar para a página da Lojinha Web
+        navegador.get("http://165.227.93.41/lojinha-web/v2/");
+
+    }
+
+    @Test
+    @DisplayName("Não é permitido registrar um produto com valor igual a zero")
+    public void testNaoEhPermitidoRegistrarProdutoComValorIgualAZero(){
+        
+        String mensagemApresentada = new LoginPage(navegador)
+                .informarOUser("admin")
+                .informarASenha("admin")
+                .submeterFormularioDeLogin()
+                .acessarFormularioAdicaoNovoProduto()
+                .informarNomeDoProduto("MacBook PRO")
+                .informarValorDoProduto("000")
+                .informarCoresDoProduto("preto,branco")
+                .submbeterFormularioDeAdicaoComErro()
+                .capturarMensagemApresentada();
+
+        // Vou validar que a mensagem de erro foi apresentada
+        Assertions.assertEquals("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00", mensagemApresentada);
+    }
+
+    @AfterEach
+    public void afterEach(){
+        navegador.quit();
+    }
+}
